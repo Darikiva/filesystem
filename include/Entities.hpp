8d4c2f715b3d6fd77e72144f77faa4584bea6ada
Alexander Totsky <43824249+Velly69@users.noreply.github.com>
2021-04-18 15:38:43 +0300

    Add functions definitions, OFT and OFT entry

--- include/Entities.hpp
+++ include/Entities.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include "Disk.hpp"
 
 namespace FS {
 namespace Entity {
@@ -16,6 +17,19 @@ struct FileDescriptor
     std::uint8_t file_length;
     std::int8_t indexes[3];
 };
+
+struct OFTEntry
+{
+    char buffer[64]; // BLOCK_SIZE
+    size_t cur_pos; // initially zero
+    size_t file_descriptor_index;
+    bool isBufferChanged;
+};
+
+struct OFT {
+    std::vector<OFTEntry> entries;
+};
+
 
 } // namespace Entity
 } // namespace FS
--- include/FileSystem.hpp
+++ include/FileSystem.hpp
@@ -14,6 +14,16 @@ class FileSystem
 public:
     FileSystem(IOSystem& iosystem);
     void reset();
+
+    // created by Sanya
+    void create(const std::string& file_name);
+    void destroy(const std::string& file_name);
+    size_t open(const std::string& file_name); // return an index value for read, write, lseek, close
+    void close(size_t index);
+    void read(size_t index, char* mem_area, size_t count);
+    void write(size_t index, char* mem_area, size_t count);
+    void lseek(size_t index, size_t pos);
+    void directory();
 private:
     IOSystem& iosystem;
     Bitmap bitmap;
