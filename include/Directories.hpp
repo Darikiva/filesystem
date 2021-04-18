e13c0f36a190f869862b47e32df82ba106487c15
YanaSkyrda <yana.skirda101010@gmail.com>
2021-04-18 20:20:18 +0300

    Directories implemented

---
+++ include/Directories.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+
+#include "Disk.hpp"
+#include "Entities.hpp"
+#include "IOSystem.hpp"
+
+namespace FS {
+
+class Directories
+{
+public:
+    Directories(IOSystem& iosystem);
+    ~Directories();
+    const Entity::DirectoryEntry& get(std::size_t index);
+    void set(size_t index, const Entity::DirectoryEntry& value);
+    void reset();
+    inline size_t size() const { return Disk::BLOCK_SIZE / directory_size; }
+
+private:
+    std::vector<Entity::DirectoryEntry> data;
+    IOSystem& iosystem;
+    bool first_use;
+    void loadDirectories();
+    void unloadDirectories();
+    void unloadDirectory(size_t index);
+    static constexpr size_t directory_size = sizeof(Entity::DirectoryEntry);
+};
+
+} // namespace FS
