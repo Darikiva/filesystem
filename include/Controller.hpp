0c3cf78d33841c7617f86278a0022b6c84c52724
darik <darikviaa@gmail.com>
2021-04-21 23:20:06 +0300

    Implement draft version of parsing commands

--- include/Controller.hpp
+++ include/Controller.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <memory>
+#include <string>
+#include <unordered_map>
 
 #include "Disk.hpp"
 #include "FileSystem.hpp"
@@ -11,8 +13,29 @@ namespace FS {
 class Controller
 {
 public:
+    enum class Command
+    {
+        Create,
+        Destroy,
+        Open,
+        Close,
+        Read,
+        Write,
+        Seek,
+        Directory,
+        Save,
+        Invalid,
+    };
+    const static std::unordered_map<std::string, Command> str_to_command;
+
+public:
     Controller(std::string save_name);
     void reset();
+    void parse(std::string command);
+
+private:
+    Command convert(std::string str);
+
 private:
     std::unique_ptr<Disk> disk;
     std::unique_ptr<IOSystem> iosystem;
