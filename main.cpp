d7e7f531e72d082ad9f2b9bcdd2845586a1356b3
Darikiva <darikivaa@gmail.com>
2021-05-07 21:22:36 +0300

    Move test files to individual folder

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main()
 {
     FS::Controller controller("saving.bin");
     controller.reset();
-    std::ifstream in("test_fs_overflow.txt");
-    std::ofstream out("test_fs_overflow_output.txt");
+    std::ifstream in("test/test_end_of_file.txt");
+    std::ofstream out("test/test_end_of_file_output.txt");
     controller.work(in, out);
     in.close();
     out.close();
