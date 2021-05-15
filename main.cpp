6ffb0e1680a80fbf3770f08bcc178925adf37ef8
YanaSkyrda <yana.skirda101010@gmail.com>
2021-05-15 16:30:41 +0300

    small refactor

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
+#include <fstream>
 #include <iostream>
 #include <sstream>
-#include <fstream>
 
 #include "Controller.hpp"
-
 
 int main()
 {
     std::string IN[] = {
-        "../test/test_fs_overflow.txt",         //0
-        "../test/test_end_of_file.txt",         //1
-        "../test/test-sample.txt",              //2
-        "../test/custom-test-destroy.txt",      //3
-        "../test/test-close.txt",               //4
-        "../test/test-buffer-load.txt",         //5
-        "../test/test-rewrite.txt"              //6
+        "../test/test_fs_overflow.txt",    // 0
+        "../test/test_end_of_file.txt",    // 1
+        "../test/test-sample.txt",         // 2
+        "../test/custom-test-destroy.txt", // 3
+        "../test/test-close.txt",          // 4
+        "../test/test-buffer-load.txt",    // 5
+        "../test/test-rewrite.txt"         // 6
     };
 
     std::string OUT[] = {
@@ -24,15 +23,18 @@ int main()
         "../test/output/output-destroy.txt",
         "../test/output/output-close.txt",
         "../test/output/output-buffer-load.txt",
-        "../test/output/output-rewrite.txt"
-    };
+        "../test/output/output-rewrite.txt"};
 
     FS::Controller controller("saving.bin");
-    controller.reset();
-    std::ifstream in(IN[6]);
-    std::ofstream out(OUT[6]);
-    controller.work(in, out);
-    in.close();
-    out.close();
+    for (int i = 0; i < sizeof(IN); i++)
+    {
+        controller.reset();
+        std::ifstream in(IN[i]);
+        std::ofstream out(OUT[i]);
+        controller.work(in, out);
+
+        in.close();
+        out.close();
+    }
     return 0;
 }
