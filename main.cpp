a0e95c401625f47554adcd134d5902d6ee25270d
YanaSkyrda <yana.skirda101010@gmail.com>
2021-05-15 12:22:32 +0300

    scenario for destroy check added

--- main.cpp
+++ main.cpp
@@ -7,12 +7,24 @@
 
 int main()
 {
+    std::string IN[] = {
+        "../test/test_fs_overflow.txt",
+        "../test/test_end_of_file.txt",
+        "../test/test-sample.txt",
+        "../test/custom-test-destroy.txt"
+    };
+
+    std::string OUT[] = {
+        "../test/test_fs_overflow_output.txt",
+        "../test/test_end_of_file_output.txt",
+        "../test/output-sample.txt",
+        "../test/output-destroy.txt"
+    };
+
     FS::Controller controller("saving.bin");
     controller.reset();
-//    std::ifstream in("test/test_end_of_file.txt");
-//    std::ofstream out("test/test_end_of_file_output.txt");
-    std::ifstream in("../test/test-sample.txt");
-    std::ofstream out("../test/output-sample.txt");
+    std::ifstream in(IN[1]);
+    std::ofstream out(OUT[1]);
     controller.work(in, out);
     in.close();
     out.close();
